457fdfa556f839007b093f7f64f6aae8dee9ce70
GLV-off <glv.off@mail.ru>
2025-12-13 00:52:44 +0300

    New: Testing environment and fakes units

---
+++ test/src/Test.Env.pas
@@ -0,0 +1,40 @@
+﻿unit Test.Env;
+
+{$I 'glv_openapi_test.inc'}
+
+interface
+
+type
+  {
+   Окружение тестового приложения
+  }
+  TEnv = record
+    class function GetRoot: UnicodeString; static;
+
+    class function GetAssetDir: UnicodeString; static;
+
+    class function GetAsset(const APath: UnicodeString): UnicodeString; static;
+  end;
+
+implementation
+
+uses
+  SysUtils;
+
+class function TEnv.GetRoot: UnicodeString;
+begin
+  Result := UTF8Decode(ExtractFilePath(ParamStr(0)));
+end;
+
+class function TEnv.GetAssetDir: UnicodeString;
+begin
+  Result := GetRoot() + '..\..\..\Assets\';
+end;
+
+class function TEnv.GetAsset(const APath: UnicodeString): UnicodeString;
+begin
+  Result := GetAssetDir() + APath;
+end;
+
+end.
+
---
+++ test/src/Test.Fakes.pas
@@ -0,0 +1,73 @@
+﻿unit Test.Fakes;
+
+{$I 'glv_openapi_test.inc'}
+
+interface
+
+uses
+  FpJson;
+
+const
+  TEST_ASSET_FAKE_SPEC_JSON: UnicodeString = 'fake_spec.json';
+
+function GetFakeJsonSpec: TJSONObject;
+
+function CreateFakeInfoJson: TJSONObject;
+
+function CreateFakeEmptyJsonObject: TJSONObject;
+
+implementation
+
+uses
+  SysUtils,
+  Classes,
+  JsonParser,
+  Test.Env;
+
+function GetFakeJsonSpec: TJSONObject;
+
+var
+  Path: UnicodeString;
+  Stream: TFileStream;
+  JsonValue: TJSONData;
+begin
+  Path := TEnv.GetAsset(TEST_ASSET_FAKE_SPEC_JSON);
+  Stream := TFileStream.Create(UTF8Encode(Path), fmOpenRead);
+  try
+    try
+      { todo: skip UTF8_Bom }
+      JsonValue := GetJSON(Stream, True);
+    except
+      on E: Exception do
+      begin
+        JsonValue := nil;
+      end;
+    end;
+  finally
+    FreeAndNil(Stream);
+  end;
+
+  if not Assigned(JsonValue) then
+    Exit(TJSONObject.Create());
+
+  if JsonValue.InheritsFrom(TJSONObject) then
+    Result := TJSONObject(JsonValue)
+  else
+    Result := TJSONObject.Create();
+end;
+
+function CreateFakeInfoJson: TJSONObject;
+begin
+  Result := TJSONObject.Create;
+  Result.Add('title', 'fake_title');
+  Result.Add('version', '1.0.0');
+  Result.Add('description', 'fake_description');
+end;
+
+function CreateFakeEmptyJsonObject: TJSONObject;
+begin
+  Result := TJSONObject.Create;
+end;
+
+end.
+
